f23999b9148f86183faa3bea3450b518ce43ca4c
Arnou <arnoujapuao9@gmail.com>
2023-03-15 22:55:10 +0300

    Homework4

--- 5/Task4.py
+++ 5/Task4.py
@@ -1,14 +1,14 @@
-teachers = []
+professors = []
 
 a = input('Записать нового учителя(да/нет)').lower()
 
 while a != 'нет':
-    teacher = []
+    professor = []
     if a == 'да':
-        teacher.append(input('Фамилия преподавателя: '))
-        teacher.append(input('Должность: '))
-        teacher.append(input('Количество студентов во всех группах(например: 12,8,10): '))
-        teachers.append(teacher)
-    print(teachers)
+        professor.append(input('Фамилия преподавателя: '))
+        professor.append(input('Должность: '))
+        professor.append(input('Количество студентов во всех группах(например: 12,8,10): '))
+        professors.append(professor)
+    print(professors)
     a = input('Записать нового учителя(да/нет)').lower()
 
